af41f73c64daca58528bebe00f47c60254068634
Andrea Restelli <andrea.restelli@nextre.it>
2020-01-05 11:46:11 +0100

    registration html added

--- aereo-client-side/src/app/pages/registration/registration.component.ts
+++ aereo-client-side/src/app/pages/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  surname: string;
+  dateOfBirth: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  submitForm() {
   }
 
 }
