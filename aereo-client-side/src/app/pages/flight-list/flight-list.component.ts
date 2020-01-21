7ff71b5baaa180e824971e6862388cdbbdabfd59
Andrea Feltrin <a.feltrin3@campus.unimib.it>
2020-01-21 16:30:29 +0100

    aggiunga tabella dinamica lista voli e classe ListaVoli

--- aereo-client-side/src/app/pages/flight-list/flight-list.component.ts
+++ aereo-client-side/src/app/pages/flight-list/flight-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, PipeTransform, Pipe} from '@angular/core';
+
+
+import {FlightList} from './flight'; // Importa classe voli
+
 
 @Component({
   selector: 'app-flight-list',
@@ -7,7 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightListComponent implements OnInit {
 
-  constructor() { }
+  data = [ // Test data
+    {
+      id: '2783',
+      posti: 18,
+      LP: 'Milano',
+      LA: 'Mosca',
+      DP: '08/09/2011', // non ho trovato format Data
+      OP: '09:30',
+      state: true, // false = Effettuato/cancellato, quindi non mostra in lista
+    },
+    {
+      id: '1880',
+      posti: 2,
+      LP: 'Tokyo',
+      LA: 'New York',
+      DP: '11/09/2011', // non ho trovato format Data
+      OP: '20:30',
+      state: true, // false = Effettuato/cancellato, quindi non mostra in lista
+    },
+    {
+      id: '2827',
+      posti: 12,
+      LP: 'New York',
+      LA: 'Milano',
+      DP: '15/07/2011', // non ho trovato format Data
+      OP: '6:00',
+      state: false, // false = Effettuato/cancellato, quindi non mostra in lista
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
