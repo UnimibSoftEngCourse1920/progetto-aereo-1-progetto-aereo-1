8df037707c2cde26e2376ccc254c048d7dee0805
Andrea Feltrin <a.feltrin3@campus.unimib.it>
2020-01-28 17:13:43 +0100

    Tweak pagine html, pagina promozioni, compra biglietti e login

--- aereo-client-side/src/app/pages/login/login.component.ts
+++ aereo-client-side/src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../class-for-entity/User';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  user: User = new User();
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  submitForm() {
+    this.user.email = this.email;
+    this.user.password = this.password;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    this.http.post(environment.apiUrl + '/register', this.user, {headers, observe: 'response'}).subscribe(response => {
+      this.router.navigate(['/home']);
+    }, err => {
+      console.log('err', err);
+    });
+
+
   }
 
 }
