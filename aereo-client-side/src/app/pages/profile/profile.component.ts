f45b3c73922b308569eeccb14f89fa929fd8947d
Andrea Feltrin <a.feltrin3@campus.unimib.it>
2020-02-02 16:30:48 +0100

    aggiustamenti html vari

--- aereo-client-side/src/app/pages/profile/profile.component.ts
+++ aereo-client-side/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {User} from '../../class-for-entity/User';
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +9,7 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  loggedUser: any;
+  loggedUser;
   ticketsList: any = [];
   flight;
 
